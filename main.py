e40e985c23ae3ae098717c97f63d1bc9da3d30bc
Patrick Troy <ptroy@pkwy.k12.mo.us>
2021-08-03 09:57:15 -0500

    Created an MVP

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.secret_key = 'K>~EEAnH_x,Z{q.43;NmyQiNz1^Yr7'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        guess = request.form['guess']
-        message = guess
-        low_value = 1
-        high_value = 50
         still_guessing = True
+        guess = int(request.form['guess'])
+        if guess < session["magic_number"]:
+            session['low_value']=guess
+            message = f"Your guess of {guess} is too low"
+        elif guess > session["magic_number"]:
+            session['high_value'] = guess
+            message = f"Your guess of {guess} is too high"
+        elif guess == session["magic_number"]:
+            message  = f"Congratulations! You guessed the number"
+            still_guessing = False
+        
     else:
-        low_value = 1
-        high_value = 50
-        magic_number = random.randint(low_value, high_value)
+        session['low_value'] = 1
+        session['high_value'] = 50
+        session["magic_number"] = random.randint(session['low_value'], session['high_value'])
         still_guessing = True
 
         message = ''
 
-    return render_template('index.html', message = message,
-        low_value = low_value, high_value = high_value, still_guessing = still_guessing)
+    return render_template('index.html', message = message, still_guessing = still_guessing)
 
 if __name__ == '__main__':
     app.run()
